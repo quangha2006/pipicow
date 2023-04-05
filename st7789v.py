2c68c2e884b994480f2555639ebcf7a8fdce1059
Quang <hdquang206@gmail.com>
2023-04-06 03:24:04 +0700

    - Add ens160

--- st7789v.py
+++ st7789v.py
@@ -4,10 +4,12 @@ from time import sleep
 import secrets
 import network
 from picozero import pico_temp_sensor, pico_led
+from PiicoDev_ENS160 import PiicoDev_ENS160
 import ntptime
 import time
 from ds3231_port import DS3231
 import utime
+import machine
 
 BACKLIGHT_PIN = 10
 RESET_PIN = 11
@@ -121,20 +123,41 @@ def MonthFromInt(monthinyear):
         
 def Render(tft, text, screen_x, screen_y, font_size = font, color = [255,255,255], bg = [0,0,0]):
     tft.text(font_size, text, screen_x, screen_y, st7789.color565(color[0],color[1],color[2]), st7789.color565(bg[0],bg[1],bg[2]))
-
-def GetTime(tft, ip, run_button):
-    tft.fill(st7789.BLACK)
+    
+def RenderRec(tft, x, y, width, height, color = [255,255,255]):
+    tft.fill_rect(x, y, width, height, st7789.color565(color[0],color[1],color[2]))
+    
+def InitTime(ip):
     if ip != '':
         ntptime.host = "1.europe.pool.ntp.org"
         ntptime.settime()
         ds3231.save_time()
     else:
         ds3231.get_time(True)
-    currentTime = None
+        
+    rtc=machine.RTC()
+    utcNow=utime.time()
+    
+    # 7 hours = 25200 seconds# for gmt. For me gmt+7. 
+    gmt=utcNow+25200
+    
+    # for second to convert time
+    (year, month, mday, hour, minute, second, weekday, yearday)=utime.localtime(gmt)
+    # first 0 = week of year
+    # second 0 = milisecond
+    rtc.datetime((year, month, mday, 0, hour, minute, second, 0))
+def InitSensor():
+    scl=Pin(7)
+    sda=Pin(6)
+    sensor = PiicoDev_ENS160(bus=1, scl=scl, sda=sda, freq=100000)
+    return sensor
+    
+def GetTime(tft, ip, run_button,ens160):
+    tft.fill(st7789.BLACK)
+    #Screen not change
+    RenderRec(tft,0,0,240,32,[134,156,152])
+    Render(tft, "QUANG HA", 60, 0, font_L_B, [255,255,255], [134,156,152])
     while True:
-        #Render(tft, "            ", 0, 0, font_L_B, [134,156,152], [134,156,152])
-        tft.fill_rect(0,0,240,32,st7789.color565(134,156,152))
-        Render(tft, "QUANG HA", 60, 0, font_L_B, [255,255,255], [134,156,152])
         currentTime = utime.localtime()
         #format: web, 25 May, 2023
         date_string = "{},{} {},{}".format(WeekFromInt(currentTime[6]),currentTime[2], MonthFromInt(currentTime[1]), currentTime[0])
@@ -153,11 +176,25 @@ def GetTime(tft, ip, run_button):
         voltage = sensorValue * (3.3 / 65535) * 5.0
         volString = "Voltage: {:.2f}".format(voltage)
         Render(tft, volString, 10, 180)
+        # Read from the sensor
+        aqi = ens160.aqi
+        tvoc = ens160.tvoc
+        eco2 = ens160.eco2
+        AQI = "AQI: {} [{}]".format(str(aqi.value),str(aqi.rating))
+        TVOC = "TVOC: {} ppb".format(str(tvoc))
+        eCO2 = "eCO2: {} ppm [{}]".format(str(eco2.value), str(eco2.rating))
+        Status = "Status: {}".format(ens160.operation)
+        Render(tft, AQI, 10, 198)
+        Render(tft, TVOC, 10, 216)
+        Render(tft, eCO2, 10, 234)
+        Render(tft, Status, 10, 252)
         sleep(1.0)
         if run_button.value() == 1:
             return
         
 def Start(run_button):
     tft = InitDisplay()
+    ens160 = InitSensor()
     ip = connectWifi(tft)
-    GetTime(tft, ip, run_button)
+    InitTime(ip)
+    GetTime(tft, ip, run_button, ens160)
