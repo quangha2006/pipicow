f4d2bb4e78054bc9c5da2c0aac0276b0a19da6ab
Quang <hdquang206@gmail.com>
2023-04-20 03:21:49 +0700

    - Update

--- st7789v.py
+++ st7789v.py
@@ -11,6 +11,8 @@ from ds3231_port import DS3231
 import utime
 import machine
 import aht
+from bmp280 import BMP280
+import bmp280
 
 BACKLIGHT_PIN = 10
 RESET_PIN = 11
@@ -29,6 +31,12 @@ import vga2_8x16 as font
 import vga2_16x32 as font_L
 import vga2_bold_16x32 as font_L_B
 import vga2_16x16 as font_M
+
+common_scl=Pin(5)
+common_sda=Pin(4)
+common_freq=400000
+common_id=0
+common_i2c=I2C(id=common_id,scl=common_scl, sda=common_sda, freq=common_freq)
 
 def connectWifi(tft):
     #Connect to WLAN
@@ -148,23 +156,22 @@ def InitTime(ip):
     # second 0 = milisecond
     rtc.datetime((year, month, mday, 0, hour, minute, second, 0))
 def InitSensor():
-    scl=Pin(7)
-    sda=Pin(6)
-    sensor = PiicoDev_ENS160(bus=1, scl=scl, sda=sda, freq=100000)
+    sensor = PiicoDev_ENS160(bus=common_id, scl=common_scl, sda=common_sda, freq=common_freq)
     return sensor
 
 def InitBMP280():
-    bus = I2C(0,scl=Pin(1),sda=Pin(0),freq=200000)
-    bmp = BMP280(bus)
-    bmp.use_case(BMP280_CASE_INDOOR)
+    bmp = BMP280(common_i2c)
+    bmp.use_case(bmp280.BMP280_CASE_INDOOR)
     return bmp
     
-def GetTime(tft, ip, run_button,ens160,aht_sensor):
+def RenderScreen(tft, ip, run_button,ens160,aht_sensor,bmp_sensor):
     tft.fill(st7789.BLACK)
     #Screen not change
     RenderRec(tft,0,0,240,32,[134,156,152])
     Render(tft, "QUANG HA", 60, 0, font_L_B, [255,255,255], [134,156,152])
     while True:
+        beginy=100
+        space=18
         currentTime = utime.localtime()
         #format: web, 25 May, 2023
         date_string = "{},{} {},{}".format(WeekFromInt(currentTime[6]),currentTime[2], MonthFromInt(currentTime[1]), currentTime[0])
@@ -174,15 +181,15 @@ def GetTime(tft, ip, run_button,ens160,aht_sensor):
         Render(tft, date_string, 0, 78, font_M)
         #Render(tft, year_string, 120, 138, font_M)
         temp0 = "Temp0: {}".format(ds3231.get_temperature())
-        Render(tft, temp0, 10, 144)
+        
         ADC_voltage = analogIn4.read_u16() * (3.3 / (65535))
         temperature_celcius = 27 - (ADC_voltage - 0.706)/0.001721
         temp1 = "Temp1: {:.2f}".format(temperature_celcius)
-        Render(tft, temp1, 10, 162)
+        
         sensorValue = analogIn0.read_u16()
         voltage = sensorValue * (3.3 / 65535) * 5.0
         volString = "Voltage: {:.2f}".format(voltage)
-        Render(tft, volString, 10, 180)
+        
         # Read from the sensor
         ens160.temperature = aht_sensor.temperature
         ens160.humidity = aht_sensor.humidity
@@ -195,34 +202,43 @@ def GetTime(tft, ip, run_button,ens160,aht_sensor):
         Status = "Status: {}".format(ens160.operation)
         temperature = "Temperature: {} ".format(ens160.temperature)
         humidity = "humidity: {}".format(ens160.humidity)
-
-        Render(tft, AQI, 10, 198)
-        Render(tft, TVOC, 10, 216)
-        Render(tft, eCO2, 10, 234)
-        Render(tft, Status, 10, 252)
-        Render(tft, temperature, 10, 270)
-        Render(tft, humidity, 10, 288)
+        
         # ReadBMP
-        #pressure=bmp.pressure
-        #p_bar=pressure/100000
-        #p_mmHg=pressure/133.3224
-        #temperature=bmp.temperature
-        #print("Temperature: {} C".format(temperature))
-        #print("Pressure: {} Pa, {} bar, {} mmHg".format(pressure,p_bar,p_mmHg))    pressure=bmp.pressure
-        #p_bar=pressure/100000
-        #p_mmHg=pressure/133.3224
-        #temperature=bmp.temperature
-        #print("Temperature: {} C".format(temperature))
-        #print("Pressure: {} Pa, {} bar, {} mmHg".format(pressure,p_bar,p_mmHg))
+        pressure=bmp_sensor.pressure
+        p_bar=pressure/100000
+        p_mmHg=pressure/133.3224
+        temperature_bmp=bmp_sensor.temperature
+        bmp_tem = "Temp_bmp: {} C".format(temperature_bmp)
+        bmp_pre = "Pressure: {} Pa, {} bar, {} mmHg".format(pressure,p_bar,p_mmHg)
+        
+        Render(tft, temp0, 10, beginy)
+        Render(tft, temp1, 10, beginy + space * 1)
+        Render(tft, volString, 10, beginy + space* 2)
+        Render(tft, AQI, 10,beginy + space* 3)
+        Render(tft, TVOC, 10, beginy + space* 4)
+        Render(tft, eCO2, 10, beginy + space* 5)
+        Render(tft, Status, 10, beginy + space* 6)
+        Render(tft, temperature, 10, beginy + space* 7)
+        Render(tft, humidity, 10, beginy + space* 8)
+        Render(tft, bmp_tem, 10, beginy + space* 9)
+        Render(tft, bmp_pre, 10, beginy + space* 10)
         sleep(1.0)
         if run_button.value() == 1:
             return
         
 def Start(run_button):
+    print("Init Display")
     tft = InitDisplay()
-    ens160 = InitSensor()
-    aht_sensor = aht.AHT2x(I2C(id=1,scl=Pin(7), sda=Pin(6), freq=100000), crc=True)
-    #bmp = InitBMP280()
+    print("Connect Wifi")
     ip = connectWifi(tft)
+    print("Init Time")
     InitTime(ip)
-    GetTime(tft, ip, run_button, ens160, aht_sensor)
+    print("Init BMP280")
+    bmp_sensor = InitBMP280()
+    print("Init AHT21")
+    aht_sensor = aht.AHT2x(common_i2c, crc=True)
+    print("Init ENS160")
+    ens160 = InitSensor()
+    print("All Done")
+    RenderScreen(tft, ip, run_button, ens160, aht_sensor, bmp_sensor)
+
